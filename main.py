0e7b91816ba507d545c1b1740b91984ccb7abeb7
nissy-shota <shota_4410@icloud.com>
2022-05-24 19:57:24 +0900

    training and test

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import hydra
 import mlflow
 import torch
+import torch.optim as optim
+import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from dotenv import load_dotenv
@@ -11,6 +13,7 @@ from mlflow import log_artifacts, log_metric, log_param
 from omegaconf import DictConfig
 
 from notifications import notification
+from models import simplenet
 
 
 logger = logging.getLogger(__name__)
@@ -20,7 +23,82 @@ logger.setLevel(logging.DEBUG)
 @hydra.main(config_path="configs/", config_name="config.yaml")
 def main(cfg: DictConfig) -> None:
 
-    
+    # define transform
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+    # create dataset and dataloader
+    trainset = torchvision.datasets.CIFAR10(
+        root="./data", train=True, download=True, transform=transform
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=cfg.batch_size, shuffle=True, num_workers=2
+    )
+
+    testset = torchvision.datasets.CIFAR10(
+        root="./data", train=False, download=True, transform=transform
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=cfg.batch_size, shuffle=False, num_workers=2
+    )
+
+    # cifar10 classes
+    classes = (
+        "plane",
+        "car",
+        "bird",
+        "cat",
+        "deer",
+        "dog",
+        "frog",
+        "horse",
+        "ship",
+        "truck",
+    )
+
+    net = simplenet.Net()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=cfg.learning_rate, momentum=cfg.momentum)
+
+    for epoch in range(cfg.epochs):  # loop over the dataset multiple times
+
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:  # print every 2000 mini-batches
+                print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+                running_loss = 0.0
+
+    print("Finished Training")
+
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            # calculate outputs by running images through the network
+            outputs = net(images)
+            # the class with the highest energy is what we choose as prediction
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f"Accuracy of the network on the 10000 test images: {100 * correct // total}")
 
 
 if __name__ == "__main__":
