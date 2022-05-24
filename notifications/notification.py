7fad773babcb1675384d3b1a3ed3c6cebcf2ec97
nissy-shota <shota_4410@icloud.com>
2022-05-24 21:16:56 +0900

    notification running place

--- notifications/notification.py
+++ notifications/notification.py
@@ -1,12 +1,26 @@
+import os
+from platform import node
+
 from matplotlib.pyplot import text
 from slackweb import Slack
 
 
 class slack_notification:
-    def __init__(self, slack_webhook_url: str) -> None:
+    def __init__(self, slack_webhook_url: str, home: str, univ: str) -> None:
 
         self.slack_webhook_url = slack_webhook_url
         self.slack = Slack(url=self.slack_webhook_url)
 
+        node_name = os.uname().nodename
+
+        if node_name == home:
+            self.place = "home"
+        elif node_name == univ:
+            self.place = "univ"
+        else:
+            self.place = "where?"
+
     def send_message(self, message: str = "debug") -> None:
+
+        message += f" running in {self.place}"
         self.slack.notify(text=message)
